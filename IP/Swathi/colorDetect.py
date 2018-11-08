11b7e5225fe4daf8c63bbe4e43566ab30dc5839e
Swathi <42289368+nrswathi@users.noreply.github.com>
2018-11-08 19:10:45 +0530

    Update colorDetect.py

--- IP/Swathi/colorDetect.py
+++ IP/Swathi/colorDetect.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 image=cv2.imread("color.png")
 hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-lower_red=np.array([20,50,50])
-upper_red=np.array([255,255,255])
-mask=cv2.inRange(hsv, lower_red, upper_red)
+lower=np.array([20,50,50])# appropriate lower margin
+upper=np.array([255,255,255])# appropriate upper margin
+mask=cv2.inRange(hsv, lower, upper)
 res = cv2.bitwise_and(image,image, mask= mask)
 
-cv2.imshow("Original Image",image)
-cv2.imshow("Detection",res)
+cv2.imshow("Original",image)
+cv2.imshow("Final",res)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
