93259cd020d6c61a3e434f3c78878018d18b89bc
NayanaBannur <moonlace45@gmail.com>
2019-02-16 18:46:05 +0530

    Update create_test_data

--- IP/Nayana/create_test_data.py
+++ IP/Nayana/create_test_data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 odlc_data = np.empty((0,5),dtype=str)
 
-#path, shape colour, letter colour, shape
+#path, letter colour, letter, chape colour, shape
 
 odlc_data = np.append(odlc_data, [['pic1.JPG', 'blue', 'T', 'red', 'semicircle']], axis=0)
 odlc_data = np.append(odlc_data, [['pic2.JPG', 'white', 'V', 'blue', 'square']], axis=0)
@@ -10,5 +10,5 @@ odlc_data = np.append(odlc_data, [['pic3.JPG', 'white', 'S', 'gray', 'circle']],
 odlc_data = np.append(odlc_data, [['pic4.JPG', 'blue', 'O', 'white', 'triangle']], axis=0)
 odlc_data = np.append(odlc_data, [['pic5.JPG', 'white', 'B', 'green', 'triangle']], axis=0)
 
-np.save('data1',odlc_data)
+np.save('data',odlc_data)
 
